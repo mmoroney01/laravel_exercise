cd1d38dbe6f856d20b8b3ef7b2516d319248d5a8
Michael Moroney <mmoroney06@gmail.com>
2018-04-16 09:05:29 -0500

    implement arrows to display price over time

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,29 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\Psr6CacheStorage;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
+use Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy;
 
 class IndexController extends Controller
 {
 	public function show()
     {
         // Retrieve information about the bitcoin currency
-        $bitcoinInfo = $this->getCryptoCurrencyInformation("bitcoin");
-
-
-        // And so on with more than 1010 cryptocurrencies ...
-
+        $bitcoinInfo = $this->getCryptoCurrencyInformation("bitcoin", "EUR");
         // Return a view as response (default.blade.php)
         return view("index", [
             "bitcoin" => $bitcoinInfo
         ]);
     }
 
-    private function getCryptoCurrencyInformation($currencyId, $convertCurrency = "USD"){
+    private function getCryptoCurrencyInformation($currencyId, $currencyType)
+    {
         // Create a new Guzzle Plain Client
         $client = new Client();
 
         // Define the Request URL of the API with the providen parameters
-        $requestURL = "https://api.coinmarketcap.com/v1/ticker/$currencyId/?convert=$convertCurrency";
+        $requestURL = "https://api.coinmarketcap.com/v1/ticker/$currencyId/?convert=$currencyType";
 
         // Execute the request
         $singleCurrencyRequest = $client->request('GET', $requestURL);
@@ -41,6 +44,42 @@ class IndexController extends Controller
 
         // Returns the array with information about the desired currency
         return $body;
+    }
+
+     private function getGuzzleFileCachedClient(){
+        // Create a HandlerStack
+        $stack = HandlerStack::create();
+
+        // 10 minutes to keep the cache
+        $TTL = 600;
+
+        // Retrieve the cache folder path of your Laravel Project
+        $cacheFolderPath = base_path() . "/bootstrap";
+        
+        // Instantiate the cache storage: a PSR-6 file system cache with 
+        // a default lifetime of 10 minutes (60 seconds).
+        $cache_storage = new Psr6CacheStorage(
+            new FilesystemAdapter(
+                // Create Folder GuzzleFileCache inside the providen cache folder path
+                'GuzzleFileCache',
+                $TTL, 
+                $cacheFolderPath
+            )
+        );
+        
+        // Add Cache Method
+        $stack->push(
+            new CacheMiddleware(
+                new GreedyCacheStrategy(
+                    $cache_storage,
+                    600 // the TTL in seconds
+                )
+            ), 
+            'greedy-cache'
+        );
+        
+        // Initialize the client with the handler option and return it
+        return new Client(['handler' => $stack]);
     }
 
     // public function show(){
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.app')
 
+@section('content')
+	<ul>
+	Current Price of Bitcoin (USD): <b>$ {{ $bitcoin["price_usd"] }}</b> <br>
+
+	Price of Bitcoin over 1 Hour (USD): <b>$ {{ $bitcoin["percent_change_1h"] }}</b> 
+
+	@if($bitcoin["percent_change_1h"] > 0)
+	  <span>&#8593;</span>
+	@elseif($bitcoin["percent_change_1h"] < 0)
+	  <span>&#8595;</span>
+	@endif
+
+	<br>
+
+	Price of Bitcoin over 1 Day (USD): <b>$ {{ $bitcoin["percent_change_24h"] }}</b> 
+
+	@if($bitcoin["percent_change_24h"] > 0)
+	  <span>&#8593;</span>
+	@elseif($bitcoin["percent_change_24h"] < 0)
+	  <span>&#8595;</span>
+	@endif
+
+	<br>
+
+	Price of Bitcoin over 1 Week (USD): <b>$ {{ $bitcoin["percent_change_7d"] }}</b>
+
+	@if($bitcoin["percent_change_7d"] > 0)
+	  <span>&#8593;</span>
+	@elseif($bitcoin["percent_change_7d"] < 0)
+	  <span>&#8595;</span>
+	@endif
+	<br>
+	</ul>
+@endsection
+
 <!DOCTYPE html>
 <html>
 
@@ -9,12 +44,8 @@
 
 <body>
 
-Price of the Bitcoin (USD): <b>$ {{ $bitcoin["price_usd"] }}</b> <br>
 
-{{-- Dump the entire arrays into the view --}}
-{{dd($bitcoin)}}
-
-{{dd($ethereum)}}
 
 </body>
 </html>
+
